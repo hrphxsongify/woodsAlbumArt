94558191cc2553ecab042b4f2f998dcbe9dec3e6
Max <maxmillianrwoods@gmail.com>
2019-07-08 16:22:33 -0700

    Repository set up with ESLint

---
+++ client/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    console.log(this);
+    return (
+      <div>
+        <h1>hi</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static(__dirname + '/../client'));
+
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
