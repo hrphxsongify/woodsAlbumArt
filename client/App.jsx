02a6b85d3752a455bd09474ffe7175c8b32a1ad2
Max Woods <47185508+maxmillian-woods@users.noreply.github.com>
2019-07-20 14:40:33 -0700

    Update App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -15,7 +15,7 @@ class App extends React.Component {
     const url = window[window.length - 1];
     $.ajax({
       type: 'get',
-      url: `ec2-34-211-225-3.us-west-2.compute.amazonaws.com/api/album/${url}`,
+      url: `http://ec2-34-211-225-3.us-west-2.compute.amazonaws.com/api/album/${url}`,
       success: data => {
         this.setState({ url: data[0].img });
       },
